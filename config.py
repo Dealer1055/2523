2ea27907842d5a7846f53cbb1c01cccd2bd08dc3
Dealer1055 <Dealer1055@abb923609>
2025-05-30 09:55:20 +0500

    n

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 BOT_TOKEN = '8075387258:AAH0dvxwNXYsK_oRFmOY5jF2N8T4EVqmX8Y'
 CHANNELS_URL = ['', 
                 'https://t.me/+MV-hkEgCpa1iMmQy',
+                'https://t.me/+QX5U8I6cMfBhYmFi',
                 'https://t.me/+jXTBpEFqYcsxZjUy']
 CHANNELS_ID = ['-1002686138511', '-1002645836804', '-1002685331478']
