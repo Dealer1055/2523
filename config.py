af37f27dac5bb79eb20f4bfcacc54aa79c8f8b1e
Dealer1055 <Dealer1055@abb923609>
2025-05-30 09:46:40 +0500

    ,

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN='7770700132:AAHyv8mMtj0jSPLD7pB8ec-YLJYHYI1-d1g'
-CHANNELS_URL = ['https://t.me/asilbekmirolimov', 
-                'https://t.me/Abduqahhor_is_history',
-                'https://t.me/trentmyuzik']
-CHANNELS_ID = ['-1001687130068', '-1002473956918', '-1002133045821']
+BOT_TOKEN='8075387258:AAE23mWmxLry6Wx3SFAEukS1pPwv6IOTt3A'
+CHANNELS_URL = ['', 
+                'https://t.me/+MV-hkEgCpa1iMmQy',
+                'https://t.me/+jXTBpEFqYcsxZjUy']
+CHANNELS_ID = ['-1002686138511', '-1002645836804', '-1002685331478']
